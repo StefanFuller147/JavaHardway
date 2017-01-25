a8dc899025a0c7ad9e44296b449dcb560bb44197
StefanFuller147 <sfullersywn@gmail.com>
2017-01-25 15:32:19 -0700

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+	public static void main ( String[] args ){
+		System.out.println ("I, Stefan Fuller, am determined to learn how to code");
+		System.out.println ("Today's date is, January 14th, 2017");
+	}
+}
+	
