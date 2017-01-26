d4e271f034e2fe6538cd2c37d54a7698df4bd611
StefanFuller147 <sfullersywn@gmail.com>
2017-01-25 18:59:49 -0700

    adding gasonline receipt exercise 2.

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+	public static void main ( String[] args) {
+		System.out.println ("+--------------------+");
+		System.out.println ("|                    |");
+		System.out.println ("|    Corner Store    |");
+		System.out.println ("|                    |");
+		System.out.println ("| 2017-01-14 12:20PM |");
+		System.out.println ("|                    |");
+		System.out.println ("| Gallons    $1.07   |");
+		System.out.println ("| Price/gallon $2.07 |");
+		System.out.println ("|                    |");
+		System.out.println ("| Fuel total: $22.91 |");
+		System.out.println ("|                    |");
+		System.out.println ("+--------------------+");
+		}
+	}
