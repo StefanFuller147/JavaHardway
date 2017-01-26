8f99e151102eab08051f4b08dfaf19eb30ca79ef
StefanFuller147 <sfullersywn@gmail.com>
2017-01-25 19:17:05 -0700

    Exercise 5

---
+++ CreatingVariables.java
@@ -0,0 +1,41 @@
+public class CreatingVariables {
+	public static void main (String [] args) {
+		int x, y, age, animals;
+		double seconds, e, checking, cats, dog;
+		String firstname, lastname, title, beer, address, cat1, cat2, dog1;
+
+		x= 10;
+		y= 400;
+		age= 25;
+		animals= 3;
+
+		seconds= 4.71;
+		e= 2.7182;
+		checking= 1.89;
+		cats= 2;
+		dog= 1;
+
+		firstname= "Stefan";
+		lastname= "Fuller";
+		title= "Mr";
+		beer= "Delish!";
+		address= "7456 E Princeton Ave";
+		cat1= "Framboise";
+		cat2= "Cola";
+		dog1= "Obie";
+
+		System.out.println ("The variable  x  contains "  +  x);
+		System.out.println ("The variable "  + y +  " Is stored in the variable y");
+		System.out.println ("I am " + age + " years old");
+		System.out.println ("I have " + animals + " animals");
+		System.out.println (cats + " cats and " + dog + " dog");
+		System.out.println ("My cats names are " + cat1 + " and " + cat2);
+		System.out.println ("And my dogs name is " + dog1);
+		System.out.println ("The experiment took " +  seconds +  " seconds");
+		System.out.println ("A favorite irrational # is Euler's number "  + e);
+		System.out.println ("Hopefully you have more money than "  + checking);
+		System.out.println ("My name is "  + title + firstname + lastname);
+		System.out.println ("My home address is " + address);
+		System.out.println ("And I think beer is " + beer);
+		}
+	}
